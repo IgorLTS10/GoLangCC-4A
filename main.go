48e2962f995d8df1a890a424623e2cdf2b507826
IgorLTS10 <ichrzyptowicz@myges.fr>
2023-09-28 10:57:56 +0200

    Function getRepositories

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type Repository struct {
+	Name         string `json:"name"`
+	LastModified string `json:"updated_at"`
+}
+
+func main() {
+	username := "IgorLTS10"
+	token := ""
+
+}
+
+func getRepositories(username string, token string) {
+	url := fmt.Sprintln("https://api.github.com/users/%s/repos", username)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Set("Connexion", "token "+token)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	var repos []Repository
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&repos)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
