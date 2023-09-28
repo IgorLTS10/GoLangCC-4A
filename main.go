146208edae5e1c0ec322b4024ee0c0403757582a
IgorLTS10 <ichrzyptowicz@myges.fr>
2023-09-28 11:07:13 +0200

    func main

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os/exec"
+	"sort"
 )
 
 type Repository struct {
@@ -13,11 +15,28 @@ type Repository struct {
 
 func main() {
 	username := "IgorLTS10"
-	token := ""
+	token := "sss"
 
+	repos, err := getRepositories(username, token)
+	if err != nil {
+		fmt.Println("Erreur:", err)
+		return
+	}
+
+	sort.Slice(repos, func(i, j int) bool {
+		return repos[i].LastModified > repos[j].LastModified
+	})
+
+	for _, repo := range repos {
+		cloneURL := fmt.Sprintf("https://github.com/%s/%s.git", username, repo.Name)
+		err := exec.Command("git", "clone", cloneURL).Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
 
-func getRepositories(username string, token string) {
+func getRepositories(username string, token string) ([]Repository, error)) {
 	url := fmt.Sprintln("https://api.github.com/users/%s/repos", username)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
